3b48afe03c0b792e51a5e60a3b9a48524ffc4af4
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-28 00:16:17 +0300

    nineteen

---
+++ nineteen/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+string a = Console.ReadLine()!;
+char[] arr = new char[a.Length];
+if(arr.Length > 5)
+Console.Write("Введите не более 5 чисел");
+else if (arr.Length < 5)
+{Console.Write("Введите не менее 5 чисел");}
+else
+if (a[0] == a[4] && a[1] == a[3])
+    Console.Write("Число является палиндромом");
+
+else
+{
+    Console.Write("Число не является палиндромом");
+}
