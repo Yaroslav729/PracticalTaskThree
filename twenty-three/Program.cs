e3f15a43021524d71d2adc7244ffc2703125f837
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-27 22:55:31 +0300

    twenty-three

--- twenty-three/Program.cs
+++ twenty-three/Program.cs
@@ -1,2 +1,5 @@
 ﻿Console.Clear();
-Console.WriteLine("Hello, World!");
+Console.Write("Введите число: ");
+int n = int.Parse(Console.ReadLine()!);
+for(int i = 1; i <= n; i++)
+Console.Write($"{i * i * i} ");
