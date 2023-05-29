3e01932a23baab48a6dd7761db6f63ade6676394
Yaroslav Shishkin <company-rosstan@yandex.ru>
2023-05-29 17:26:14 +0300

    nineteen

---
+++ twenty-one/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+Console.Write("Введите координату X первой точки: ");
+int x1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y первой точки: ");
+int y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z первой точки: ");
+int z1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату X второй точки: ");
+int x2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Y второй точки: ");
+int y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координату Z второй точки: ");
+int z2 = int.Parse(Console.ReadLine()!);
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
